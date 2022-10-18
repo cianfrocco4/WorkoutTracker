4b6ba57fc4412c4a5b221c33b90eea51309749e7
Anthony Cianfrocco <afc4328@rit.edu>
2022-10-17 20:56:16 -0400

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Anthony Cianfrocco on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
